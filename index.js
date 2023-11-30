6f36fc5f21bc8205d9758746d45b6b9e1f276965
Alex Messina <me@alexmessina.dev>
2023-11-29 22:56:33 -0600

    Adjusted classes to correctly render images -Added attributes for text color and position -Formatted attribute values to render correctly -Adjusted index.js for testing purposes

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const baseShape = require("/lib/baseShape.js");
-const rectangle = require("/lib/rectangle.js");
-const circle = require("/lib/circle.js");
-const triangle = require("/lib/triangle.js");
-const inquirer = require("inquirer");
-const fs = require("fs");
+const BaseShape = require('./lib/baseShape.js');
+const Rectangle = require('./lib/rectangle.js');
+const Circle = require('./lib/circle.js');
+const Triangle = require('./lib/triangle.js');
+//const inquirer = require('inquirer');
+const fs = require('fs');
 
 
+shape.createShape();
+const html = shape.render();
+fs.writeFile('./Examples/triangle.svg', html, (err)=>
+    err ? console.log(err) : console.log("File created.")
+)
