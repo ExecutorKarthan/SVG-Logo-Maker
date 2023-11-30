207236c2f8e603ff2c4178dde01235be0f2a8b53
Alex Messina <me@alexmessina.dev>
2023-11-29 21:38:40 -0600

    Added more stubs -Separated classes into distinct files -Added files for Tests -Added import statements in index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const baseShape = require("/lib/baseShape.js");
+const rectangle = require("/lib/rectangle.js");
+const circle = require("/lib/circle.js");
+const triangle = require("/lib/triangle.js");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
