727c87b7e40b4086096349ec3824acca253ea67b
Alex Messina <me@alexmessina.dev>
2023-11-30 22:34:06 -0600

    Added user input -Added Inquiry package -Added Inquiry prompts to collect data -Linked user prompts to image generation process -Adjusted image formatting properties in classes

--- index.js
+++ index.js
@@ -2,12 +2,89 @@ const BaseShape = require('./lib/baseShape.js');
 const Rectangle = require('./lib/rectangle.js');
 const Circle = require('./lib/circle.js');
 const Triangle = require('./lib/triangle.js');
-//const inquirer = require('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs');
+const { error } = require('console');
+const baseShape = require('./lib/baseShape.js');
+const acceptedCharacters = "abcdefghijklmnopqrstuvwxyz0123456789#"
+
+// Create an array of questions for user input
+const questions = [
+    //Logo Text Letters
+    "What are the letters to your logo? Please enter in no more than 3!",
+    
+    //Text Color
+    "What color would you like your text? You can use lowercase english letters or by entering # followed by hexadecimal numbers.",
+    
+    //Shape
+    "What shape would you like?",
+    
+    //Shape Color
+    "What color would you like to use for your shape? You can use lowercase english letters or by entering # hexadecimal numbers.",
+    
+];
+
+function init(){
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "logoLetters",
+            message: questions[0]
+        },
+        {
+            type: "input",
+            name: "textColor",
+            message: questions[1]
+        },
+        {
+            type: "list",
+            name: "shape",
+            message: questions[2],
+            choices: ["Rectangle", "Circle", "Triangle"]
+        },
+        {
+            type: "input",
+            name: "shapeColor",
+            message: questions[3]
+        }
+    ]).then((data) =>{
+        var shape = "";
+        if(data.shape == "Rectangle"){
+            shape = new Rectangle(data.logoLetters.toUpperCase(), data.textColor, data.shapeColor)
+        }
+        else if(data.shape == "Circle"){
+            shape = new Circle(data.logoLetters.toUpperCase(), data.textColor, data.shapeColor)
+        }
+        else{
+            shape = new Triangle(data.logoLetters.toUpperCase(), data.textColor, data.shapeColor)
+        }
+        
+        shape.createShape();
+        const html = shape.render();
+        fs.writeFile('./Examples/logo.svg', html, (err)=>
+        err ? console.log(err) : console.log("File created.")
+        )
 
 
-shape.createShape();
-const html = shape.render();
-fs.writeFile('./Examples/triangle.svg', html, (err)=>
-    err ? console.log(err) : console.log("File created.")
-)
+        /*const lettersValid = false;
+        const textColorValid = false;
+        const shapeColorValid = false;
+        if(data.logoLetters.length > 1 && data.logoLetters.length < 4){
+            lettersValid == true;
+        }
+        else{
+            throw new error("You need at least 1 logo letter and no more than 3. Please reenter your logo letters.")
+        }
+        const invalidChar = true;
+        for(letter of data.textColor){
+            if(acceptedCharacters.indexOf(letter.toLowercase()) < 0){
+                invalidChar = false;
+                break
+            }                
+        }*/
+        
+    })
+}
+
+init();
